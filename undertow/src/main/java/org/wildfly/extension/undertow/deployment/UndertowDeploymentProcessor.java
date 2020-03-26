8d2efa3389b4119244f78264f35d876754bb6cf1
rmartinc <rmartinc@redhat.com>
2020-03-26 09:42:28 +0100

    [WFLY-13293] When deploying "ROOT.war" in EAP7.x, the context root value output through jboss-cli is not valid

--- undertow/src/main/java/org/wildfly/extension/undertow/deployment/UndertowDeploymentProcessor.java
+++ undertow/src/main/java/org/wildfly/extension/undertow/deployment/UndertowDeploymentProcessor.java
@@ -181,12 +181,12 @@ public class UndertowDeploymentProcessor implements DeploymentUnitProcessor, Fun
             deploymentName = deploymentUnit.getParent().getName() + "." + deploymentUnit.getName();
         }
 
-        final Map.Entry<String,String> severHost = defaultModuleMappingProvider.getMapping(deploymentName);
+        final Map.Entry<String,String> serverHost = defaultModuleMappingProvider.getMapping(deploymentName);
         String defaultHostForDeployment;
         String defaultServerForDeployment;
-        if (severHost != null) {
-            defaultServerForDeployment = severHost.getKey();
-            defaultHostForDeployment = severHost.getValue();
+        if (serverHost != null) {
+            defaultServerForDeployment = serverHost.getKey();
+            defaultHostForDeployment = serverHost.getValue();
         } else {
             defaultServerForDeployment = this.defaultServer;
             defaultHostForDeployment = this.defaultHost;
@@ -194,8 +194,10 @@ public class UndertowDeploymentProcessor implements DeploymentUnitProcessor, Fun
 
         String serverInstanceName = warMetaData.getMergedJBossWebMetaData().getServerInstanceName() == null ? defaultServerForDeployment : warMetaData.getMergedJBossWebMetaData().getServerInstanceName();
         String hostName = hostNameOfDeployment(warMetaData, defaultHostForDeployment);
+        // flag if the app is the default web module for the server/host
+        boolean isDefaultWebModule = serverHost != null && serverInstanceName.equals(defaultServerForDeployment) && hostName.equals(defaultHostForDeployment);
 
-        processDeployment(warMetaData, deploymentUnit, phaseContext.getServiceTarget(), deploymentName, hostName, serverInstanceName);
+        processDeployment(warMetaData, deploymentUnit, phaseContext.getServiceTarget(), deploymentName, hostName, serverInstanceName, isDefaultWebModule);
     }
 
 
@@ -215,7 +217,7 @@ public class UndertowDeploymentProcessor implements DeploymentUnitProcessor, Fun
     }
 
     private void processDeployment(final WarMetaData warMetaData, final DeploymentUnit deploymentUnit, final ServiceTarget serviceTarget,
-                                   final String deploymentName, final String hostName, final String serverInstanceName)
+                                   final String deploymentName, final String hostName, final String serverInstanceName, final boolean isDefaultWebModule)
             throws DeploymentUnitProcessingException {
         ResourceRoot deploymentResourceRoot = deploymentUnit.getAttachment(Attachments.DEPLOYMENT_ROOT);
         final VirtualFile deploymentRoot = deploymentResourceRoot.getRoot();
@@ -265,7 +267,7 @@ public class UndertowDeploymentProcessor implements DeploymentUnitProcessor, Fun
             jaccContextId = deploymentUnit.getParent().getName() + "!" + jaccContextId;
         }
 
-        final String pathName = pathNameOfDeployment(deploymentUnit, metaData);
+        String pathName = pathNameOfDeployment(deploymentUnit, metaData, isDefaultWebModule);
 
         final CapabilityServiceSupport capabilities = deploymentUnit.getAttachment(Attachments.CAPABILITY_SERVICE_SUPPORT);
         final boolean securityEnabled = capabilities.hasCapability(LEGACY_SECURITY_CAPABILITY_NAME);
@@ -516,9 +518,11 @@ public class UndertowDeploymentProcessor implements DeploymentUnitProcessor, Fun
         return this.nonDistributableSessionManagementProvider;
     }
 
-    static String pathNameOfDeployment(final DeploymentUnit deploymentUnit, final JBossWebMetaData metaData) {
+    static String pathNameOfDeployment(final DeploymentUnit deploymentUnit, final JBossWebMetaData metaData, final boolean isDefaultWebModule) {
         String pathName;
-        if (metaData.getContextRoot() == null) {
+        if (isDefaultWebModule) {
+            pathName = "/";
+        } else if (metaData.getContextRoot() == null) {
             final EEModuleDescription description = deploymentUnit.getAttachment(org.jboss.as.ee.component.Attachments.EE_MODULE_DESCRIPTION);
             if (description != null) {
                 // if there is an EEModuleDescription we need to take into account that the module name may have been overridden
