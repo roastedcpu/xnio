32198f83c5f8539745fa75bd661251111b4cba98
Darran Lofthouse <darran.lofthouse@jboss.com>
2020-10-09 09:51:32 +0100

    [JBEAP-20211] / [WFLY-13946] Eliminate a couple of remaining dependencies on legacy security where legacy security is not available.

--- ejb3/src/main/java/org/jboss/as/ejb3/component/EJBComponentDescription.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/component/EJBComponentDescription.java
@@ -125,6 +125,8 @@ import org.wildfly.security.authz.Roles;
  */
 public abstract class EJBComponentDescription extends ComponentDescription {
 
+    // references to external capabilities
+    private static final String LEGACY_SECURITY_SERVER_MANAGER_CAPABILITY_NAME = "org.wildfly.legacy-security.server-security-manager";
     private static final ServiceName SECURITY_DOMAIN_SERVICE = ServiceName.JBOSS.append("security", "security-domain");
 
     /**
@@ -295,8 +297,8 @@ public abstract class EJBComponentDescription extends ComponentDescription {
      * @param deploymentUnitServiceName
      * @param descriptorData            the optional descriptor metadata
      */
-    public EJBComponentDescription(final String componentName, final String componentClassName, final EjbJarDescription ejbJarDescription, final ServiceName deploymentUnitServiceName, final EnterpriseBeanMetaData descriptorData) {
-        super(componentName, componentClassName, ejbJarDescription.getEEModuleDescription(), deploymentUnitServiceName);
+    public EJBComponentDescription(final String componentName, final String componentClassName, final EjbJarDescription ejbJarDescription, final DeploymentUnit deploymentUnit, final EnterpriseBeanMetaData descriptorData) {
+        super(componentName, componentClassName, ejbJarDescription.getEEModuleDescription(), deploymentUnit.getServiceName());
         this.descriptorData = descriptorData;
         if (ejbJarDescription.isWar()) {
             setNamingMode(ComponentNamingMode.USE_MODULE);
@@ -304,9 +306,11 @@ public abstract class EJBComponentDescription extends ComponentDescription {
             setNamingMode(ComponentNamingMode.CREATE);
         }
 
+        final boolean legacySecurityInstalled = legacySecurityAvailable(deploymentUnit);
+
         getConfigurators().addFirst(new NamespaceConfigurator());
         getConfigurators().add(new EjbJarConfigurationConfigurator());
-        getConfigurators().add(new SecurityDomainDependencyConfigurator(this));
+        getConfigurators().add(new SecurityDomainDependencyConfigurator(this, legacySecurityInstalled));
 
 
         // setup a current invocation interceptor
@@ -321,7 +325,7 @@ public abstract class EJBComponentDescription extends ComponentDescription {
 
         //add a dependency on the module deployment service
         //we need to make sure this is up before the EJB starts, so that remote invocations are available
-        addDependency(deploymentUnitServiceName.append(ModuleDeployment.SERVICE_NAME));
+        addDependency(deploymentUnit.getServiceName().append(ModuleDeployment.SERVICE_NAME));
 
         getConfigurators().addFirst(EJBValidationConfigurator.INSTANCE);
         getConfigurators().add(new ComponentConfigurator() {
@@ -358,7 +362,7 @@ public abstract class EJBComponentDescription extends ComponentDescription {
                     if (ejbComponentDescription.getSecurityDomainServiceName() != null) {
                         final HashMap<Integer, InterceptorFactory> elytronInterceptorFactories = getElytronInterceptorFactories(policyContextID, ejbComponentDescription.requiresJacc(), true);
                         elytronInterceptorFactories.forEach((priority, elytronInterceptorFactory) -> configuration.addTimeoutViewInterceptor(elytronInterceptorFactory, priority));
-                    } else if (legacySecurityAvailable(deploymentUnit)) {
+                    } else if (legacySecurityInstalled) {
                         configuration.addTimeoutViewInterceptor(new SecurityContextInterceptorFactory(securityRequired, policyContextID), InterceptorOrder.View.SECURITY_CONTEXT);
                     }
                     final Set<Method> classMethods = configuration.getClassIndex().getClassMethods();
@@ -400,8 +404,10 @@ public abstract class EJBComponentDescription extends ComponentDescription {
         // setup ejb suspend handler dependency
         addEJBSuspendHandlerDependency();
 
-        // setup dependency on ServerSecurityManager
-        addServerSecurityManagerDependency();
+        if (legacySecurityInstalled) {
+            // setup dependency on ServerSecurityManager
+            addServerSecurityManagerDependency();
+        }
     }
 
     private static boolean legacySecurityAvailable(DeploymentUnit deploymentUnit) {
@@ -933,9 +939,11 @@ public abstract class EJBComponentDescription extends ComponentDescription {
     private class SecurityDomainDependencyConfigurator implements ComponentConfigurator {
 
         private final EJBComponentDescription ejbComponentDescription;
+        private final boolean legacySecurityInstalled;
 
-        SecurityDomainDependencyConfigurator(final EJBComponentDescription ejbComponentDescription) {
+        SecurityDomainDependencyConfigurator(final EJBComponentDescription ejbComponentDescription, final boolean legacySecurityInstalled) {
             this.ejbComponentDescription = ejbComponentDescription;
+            this.legacySecurityInstalled = legacySecurityInstalled;
         }
 
         @Override
@@ -958,7 +966,9 @@ public abstract class EJBComponentDescription extends ComponentDescription {
                             final ServiceName securityDomainServiceName = SECURITY_DOMAIN_SERVICE.append(securityDomainName);
                             serviceBuilder.requires(securityDomainServiceName);
                         }
-                        serviceBuilder.requires(SECURITY_DOMAIN_SERVICE.append(SecurityConstants.DEFAULT_EJB_APPLICATION_POLICY));
+                        if (legacySecurityInstalled) {
+                            serviceBuilder.requires(SECURITY_DOMAIN_SERVICE.append(SecurityConstants.DEFAULT_EJB_APPLICATION_POLICY));
+                        }
                     }
                 }
             });
