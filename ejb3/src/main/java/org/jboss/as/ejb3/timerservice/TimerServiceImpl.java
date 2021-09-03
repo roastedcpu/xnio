d3b14d5eca1bda1d4a04d6df50ceea1ba6c5366e
Cheng Fang <cfang@redhat.com>
2021-09-03 16:07:58 -0400

    WFLY-15192 Reduce the usage of TimeHandle in ejb timer service; and general code cleanup

--- ejb3/src/main/java/org/jboss/as/ejb3/timerservice/TimerServiceImpl.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/timerservice/TimerServiceImpl.java
@@ -597,7 +597,7 @@ public class TimerServiceImpl implements TimerService, Service<TimerService> {
         if (timer == null) {
             return;
         }
-        if (timer.isTimerPersistent()) {
+        if (timer.persistent) {
             try {
                 if (timerPersistence.getOptionalValue() == null) {
                     EJB3_TIMER_LOGGER.timerPersistenceNotEnable();
@@ -1161,7 +1161,7 @@ public class TimerServiceImpl implements TimerService, Service<TimerService> {
      */
     public boolean shouldRun(TimerImpl timer) {
         // check peristent without further check to prevent from Exception (WFLY-6152)
-        return !timer.isTimerPersistent() || timerPersistence.getValue().shouldRun(timer, ContextTransactionManager.getInstance());
+        return !timer.persistent || timerPersistence.getValue().shouldRun(timer, ContextTransactionManager.getInstance());
     }
 
     private class TimerCreationTransactionSynchronization implements Synchronization {
